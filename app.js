8da7f3f6feae9b74d96a0ca2511ee26485066151
Mr Footwork <pandau.ting@gmail.com>
2021-05-29 14:19:46 +0200

    Bücher hinzufügen

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-// const body = document.getElementsByTagName('body')[0]
+//error message
+const error = document.getElementById('error')
 
-// // <h1>test</h1>
-// const test = document.createElement('h1')
-// const testContent = test.textContent
-// test.textContent = 'test'
-
-// app.appendChild(test)
-// body.appendChild(test)
-
+//App
 const app = document.getElementById('app')
 const library = document.getElementById('library')
 
-//Input field
-const input = document.getElementById('input')
-const log = document.getElementById('values')
-input.addEventListener('input', readInput)
+//form field
+const form = document.getElementById('form')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const pages = document.getElementById('pages')
+
+function validateInput() {
+	//TODO: implement real validation
+	return true
+}
+
+function throwError() {
+	//TODO: Throw an appripriate error message
+	const message = 'Input has errors.'
+	error.innerHTML = message
+}
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	const inputsAreValid = validateInput()
+	if (!inputsAreValid) throwError()
+	addBookToLibrary(title.value, author.value, pages.value)
+})
 
 //odin project
 let myLibrary = [
@@ -38,13 +51,44 @@ function Book() {
 
 function readInput(e) {
 	log.textContent = e.target.value
+	console.log(log.textContent)
 }
 
-function addBookToLibrary() {}
-
-//display all books in the library
-;(function displayLibrary(library) {
-	library.forEach(book => {
-		console.log(book.title)
+function addBookToLibrary(title, author, pages) {
+	myLibrary.push({
+		title: title,
+		author: author,
+		pages: pages,
+		read: false,
 	})
-})(myLibrary)
+	resetDOMLibrary()
+	displayLibrary(myLibrary)
+}
+
+function resetDOMLibrary() {
+	while (library.lastElementChild) {
+		library.removeChild(library.lastElementChild)
+	}
+}
+
+function displayLibrary(libraryDB) {
+	libraryDB.forEach(book => {
+		console.log(book.title)
+		const bookCard = document.createElement('div')
+		const cardRows = document.createElement('div')
+		const title = document.createElement('h3')
+		const author = document.createElement('h2')
+		const pages = document.createElement('h2')
+		bookCard.classList.add('book')
+		cardRows.classList.add('row')
+		title.innerHTML = book.title
+		author.innerHTML = book.author
+		pages.innerHTML = book.pages
+		library.appendChild(bookCard)
+		bookCard.appendChild(cardRows)
+		cardRows.appendChild(title)
+		cardRows.appendChild(author)
+		cardRows.appendChild(pages)
+	})
+}
+displayLibrary(myLibrary)
