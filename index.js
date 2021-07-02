3610419015de4be28e3633f087878f1ca8fa82f3
Mr Footwork <pandau.ting@gmail.com>
2021-07-03 00:03:41 +0200

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import GlobalVariableContainer from './global.js'
-import * as bookTools from './book.js'
+import Book from './js/book.js'
 
 const globals = new GlobalVariableContainer()
+new Book(1, 'mein Titel', 'Pandau', 123, true)
 //TODO Authentication
 //TODO Kann man meine Bücher-ID-Übergabe optimieren?
 
-async function initializeApp() {
+async(function initializeApp() {
 	globals.snapshotLibrary = await bookTools.loadDB()
 	bookTools.resetDOMLibrary()
 	bookTools.renderAll(globals.snapshotLibrary)
-}
-initializeApp()
+})()
