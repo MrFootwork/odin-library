785ab5137d95dc2a159ba49c794a84b62181208a
Mr Footwork <pandau.ting@gmail.com>
2024-07-18 15:44:39 +0200

    update toggle read status

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,38 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-	apiKey: 'AIzaSyDJV_BSGZcTDMHriwqihtTSoNxitevkoO0',
-	authDomain: 'the-odin-project-library-91e50.firebaseapp.com',
-	projectId: 'the-odin-project-library-91e50',
-	storageBucket: 'the-odin-project-library-91e50.appspot.com',
-	messagingSenderId: '572851761208',
-	appId: '1:572851761208:web:7fd719d304747f419cf6cd',
-	measurementId: 'G-RVM02W4J3M',
-}
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  //   Timestamp,
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+// import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+  apiKey: "AIzaSyDJV_BSGZcTDMHriwqihtTSoNxitevkoO0",
+  authDomain: "the-odin-project-library-91e50.firebaseapp.com",
+  projectId: "the-odin-project-library-91e50",
+  storageBucket: "the-odin-project-library-91e50.appspot.com",
+  messagingSenderId: "572851761208",
+  appId: "1:572851761208:web:7fd719d304747f419cf6cd",
+  measurementId: "G-RVM02W4J3M",
+};
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-export const db = firebase.firestore()
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { app, db, collection, getDocs, deleteDoc, addDoc, updateDoc, doc };
+
 //without this, we get errors in the console
-db.settings({ timestampsInSnapshots: true })
+// db.settings({ timestampsInSnapshots: true })
